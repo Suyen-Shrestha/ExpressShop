b690a8b816d2b619db8c5a29edcad939fdbce088
Suyen-Shrestha <suyencrestha@gmail.com>
2020-01-09 10:16:59 +0545

    added image for items in items model

--- ExpressShop/shop/models.py
+++ ExpressShop/shop/models.py
@@ -25,6 +25,7 @@ class Item(models.Model):
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
     description = models.TextField()
+    image = models.ImageField(upload_to='items/', blank=True, null=True)
 
     def __str__(self):
         return self.title
